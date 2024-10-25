5e183d7d7e01cf3560842ddbeb213a35fcd525d6
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2024-10-25 18:00:43 +0800

    Update Tour.blade.php

--- resources/views/UserUmum/Tour.blade.php
+++ resources/views/UserUmum/Tour.blade.php
@@ -4,5 +4,5 @@
 @endsection
 @section('isi')
 
-<iframe src="{{asset('/')}}map/index.htm" width="100%" height="600px"></iframe>
+<iframe src="{{asset('/')}}map/index.htm" width="100%" height="500px"></iframe>
 @endsection
